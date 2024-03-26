b71dfb35b6ec8cf008110e8ee8231a57ac0ac160
Leander Nathan <leander.nathan@gmail.com>
2024-03-26 11:06:39 +0530

    Added search params for company, name, and featured properties

--- controllers/products.js
+++ controllers/products.js
@@ -1,19 +1,29 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
+  const search = 'acc'
   const products = await Product.find({
-    featured: true,
-    company: 'caressa',
+    name: { $regex: search, $options: 'i' },
   })
   res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query
+  const { featured, company, name } = req.query
   const queryObject = {}
+
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false
   }
+
+  if (company) {
+    queryObject.company = company
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' }
+  }
+  console.log(queryObject)
   const products = await Product.find(queryObject)
   res.status(200).json({ products, nbHits: products.length })
 }
