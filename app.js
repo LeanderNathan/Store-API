1801861c5b398cdf9a4c0da4a8a83cbf382a134f
Leander Nathan <leander.nathan@gmail.com>
2024-03-26 10:16:49 +0530

    Added product schema and async error handling via module

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+require('express-async-errors')
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+
 const connectDB = require('./db/connect')
 const productsRouter = require('./routes/products')
-require('dotenv').config()
 
 //async errors
 const notFoundMiddleware = require('./middleware/not-found')
@@ -16,12 +19,11 @@ app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">Go to Products</a>')
 })
 
-app.use('/api/v1/products', productsRouter)
 //products route
+app.use('/api/v1/products', productsRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
-
 const port = process.env.PORT || 3000
 
 const start = async () => {
