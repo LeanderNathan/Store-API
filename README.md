7e1b354621dd3dcea14f774d7853c82be2312d8e
Leander Nathan <leander.nathan@gmail.com>
2024-03-26 12:13:02 +0530

    Added numeric filers

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 An online Store API to demonstrate:
--Search Queries using params
--Filtering after validation based on Params
--Using $regex operator to filter
 
--
+- Search Queries using params
+- Filtering after validation based on Params
+- Using $regex operator to filter
+- Conditional numeric filters
+- Sorting Functionality
+- Selecting certain fields to display from each documentin DB
+- Pagination
+- Use of regular expression for userfriendly numeric sorting using >,>=,=,<,<=
 
 Technologies Used:
 
--
-
-Screenshots:
+- NodeJS : Backend
+- ExpressJS : Backend
+- Atlas MongoDB : Cloud to store DB
+- Postman : Testing
--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query
+  const { featured, company, name, sort, fields, numericFilters } = req.query
   const queryObject = {}
 
   if (featured) {
@@ -21,7 +21,30 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' }
   }
-  /* console.log(queryObject) */
+
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    }
+    const regEx = /\b(<|>|<=|>=|=)\b/g
+    let filters = numericFilters.replace(regEx, (match) => {
+      return `-${operatorMap[match]}-`
+    })
+
+    const options = ['price', 'rating']
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-')
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) }
+      }
+    })
+  }
+
+  console.log(queryObject)
   let result = Product.find(queryObject)
   //Sorting
   if (sort) {
