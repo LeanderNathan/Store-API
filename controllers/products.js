f9d950a19b778e36c5af5dc21e42c274d453a64c
Leander Nathan <leander.nathan@gmail.com>
2024-03-26 11:23:18 +0530

    Added sorting functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,12 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const search = 'acc'
-  const products = await Product.find({
-    name: { $regex: search, $options: 'i' },
-  })
+  const products = await Product.find({}).sort('name')
   res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query
+  const { featured, company, name, sort } = req.query
   const queryObject = {}
 
   if (featured) {
@@ -23,8 +20,15 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' }
   }
-  console.log(queryObject)
-  const products = await Product.find(queryObject)
+  /* console.log(queryObject) */
+  let result = Product.find(queryObject)
+  if (sort) {
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  } else {
+    result = result.sort('createdAt')
+  }
+  const products = await result
   res.status(200).json({ products, nbHits: products.length })
 }
 
