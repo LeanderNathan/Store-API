bb5ee01951606e96af618d5fc786b325f4809b66
Leander Nathan <leander.nathan@gmail.com>
2024-03-26 10:52:28 +0530

    Added QueryParams filter for getAllProducts

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,21 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: 'Product testing route' })
+  const products = await Product.find({
+    featured: true,
+    company: 'caressa',
+  })
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'Product  route' })
+  const { featured } = req.query
+  const queryObject = {}
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false
+  }
+  const products = await Product.find(queryObject)
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = {
