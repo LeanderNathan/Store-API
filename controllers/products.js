27b3eafc36dae618b41ac98eda074f669dea305b
Leander Nathan <leander.nathan@gmail.com>
2024-03-26 11:31:14 +0530

    Added functionality to select which fiels to display

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,12 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('name')
+  const products = await Product.find({}).select('name')
   res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query
+  const { featured, company, name, sort, fields } = req.query
   const queryObject = {}
 
   if (featured) {
@@ -27,6 +27,11 @@ const getAllProducts = async (req, res) => {
     result = result.sort(sortList)
   } else {
     result = result.sort('createdAt')
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList)
   }
   const products = await result
   res.status(200).json({ products, nbHits: products.length })
