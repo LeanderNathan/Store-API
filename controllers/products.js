d6da5f120a8a3c2cac0c12a1f0c10e569e904bcd
Leander Nathan <leander.nathan@gmail.com>
2024-03-26 11:43:18 +0530

    Added Pagniation

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,8 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select('name')
+  const products = await Product.find({}).sort('name').limit(10).skip(5)
+
   res.status(200).json({ products, nbHits: products.length })
 }
 
@@ -22,6 +23,7 @@ const getAllProducts = async (req, res) => {
   }
   /* console.log(queryObject) */
   let result = Product.find(queryObject)
+  //Sorting
   if (sort) {
     const sortList = sort.split(',').join(' ')
     result = result.sort(sortList)
@@ -29,10 +31,18 @@ const getAllProducts = async (req, res) => {
     result = result.sort('createdAt')
   }
 
+  //Selecting
   if (fields) {
     const fieldsList = fields.split(',').join(' ')
     result = result.select(fieldsList)
   }
+
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  result = result.skip(skip).limit(limit)
+
   const products = await result
   res.status(200).json({ products, nbHits: products.length })
 }
